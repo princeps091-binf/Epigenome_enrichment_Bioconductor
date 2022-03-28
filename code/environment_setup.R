0a521756f9380a9cab1e7123266200f20699579a
princeps091-binf <princeps091@gmail.com>
2022-03-28 14:06:36 +0200

    script for environment setup

---
+++ code/environment_setup.R
@@ -0,0 +1,15 @@
+library(renv)
+
+renv::init(bioconductor = T)
+
+renv::install("tidyverse")
+renv::install("furrr")
+renv::install("Matrix")
+renv::install("viridis")
+renv::install("seriation")
+renv::install("svglite")
+renv::install("bioc::GenomicRanges")
+renv::install("bioc::liftOver")
+renv::install("bioc::AnnotationHub")
+renv::install("bioc::rtracklayer")
+renv::snapshot()
