60a8ebac12a9e55cbfe5f3fc53b8e0b82d7c852b
princeps091-binf <princeps091@gmail.com>
2022-03-28 18:08:13 +0200

    integrate with previous results from less transparent aggregation of BigWig files at enahcer sites

--- code/integrate_GRange_BigWig.R
+++ code/integrate_GRange_BigWig.R
@@ -1,4 +1,5 @@
 library(GenomicRanges)
+library(rtracklayer)
 library(tidyverse)
 
 data_tbl_load_fn<-function(file){
@@ -9,18 +10,30 @@ data_tbl_load_fn<-function(file){
   return(out_tbl)
 }
 
-CAGE_enh_GRange_file<-"~/Documents/multires_bhicect/Bootstrapp_fn/data/GRanges/CAGE_enh_GM12878_Grange.Rda"
+CAGE_tss_GRange_file<-"~/Documents/multires_bhicect/Bootstrapp_fn/data/GRanges/CAGE_tss_HMEC_Grange.Rda"
+CAGE_enh_GRange_file<-"~/Documents/multires_bhicect/Bootstrapp_fn/data/GRanges/CAGE_enh_HMEC_Grange.Rda"
+
+CAGE_enh_tbl_file<-"~/Documents/multires_bhicect/HiC_enrichment/HiC_zscore_explore/data/HMEC_enh_H3K27ac_pval_tbl.Rda"
 
 cage_enh_GRange<-data_tbl_load_fn(CAGE_enh_GRange_file)
+cage_tss_GRange<-data_tbl_load_fn(CAGE_tss_GRange_file)
 
-bwf_bioc <-BigWigFile("~/Documents/multires_bhicect/data/epi_data/HMEC/Bioconductor/H3K27ac/hmec_H3K27ac_FC.bigWig")
-bwf_manual <-BigWigFile("~/Documents/multires_bhicect/data/epi_data/HMEC/ENCODE/H3K27ac/ENCFF981WTU_FC_HMEC_H3K27ac.bigWig")
+bwf_manual <-BigWigFile("~/Documents/multires_bhicect/data/epi_data/HMEC/CTCF/ENCODE/BigWig/ENCFF101RHP_HMEC_FC.bigWig")
 
-test_manual<-import(bwf_manual,which=cage_enh_GRange,as="NumericList")
-test_bioc<-import(bwf_bioc,which=cage_enh_GRange,as="NumericList")
+test_manual<-import(bwf_manual,which=cage_tss_GRange,as="NumericList")
+cage_enh_H3K27ac_tbl<-data_tbl_load_fn(CAGE_enh_tbl_file)
+comp_tbl<-tibble(as.data.frame(cage_enh_GRange)) %>% 
+  mutate(H3K27ac.vec=as.list(test_manual)) %>% 
+  left_join(.,cage_enh_H3K27ac_tbl %>% 
+              dplyr::select(enh,fc,pval) %>% 
+              mutate(seqnames=str_split_fixed(enh,"_",3)[,1],start=as.integer(str_split_fixed(enh,"_",3)[,2]),end=as.integer(str_split_fixed(enh,"_",3)[,3]))
+  )
 
-enh.n<-1234
-plot(seq(start(cage_enh_GRange[enh.n]),end(cage_enh_GRange[enh.n])),test[[enh.n]],type='l')
+comp_tbl %>% 
+  mutate(m=map_dbl(H3K27ac.vec,function(x)mean(x))) %>% 
+  ggplot(.,aes(m,fc))+geom_point(alpha=0.1)+scale_x_log10()+scale_y_log10()
+
+enh.n<-134
+plot(seq(start(cage_tss_GRange[enh.n]),end(cage_tss_GRange[enh.n])),test_manual[[enh.n]],type='l')
 
 
-plot(unlist(test_bioc),unlist(test_manual))
